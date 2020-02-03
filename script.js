dad7d0046d5d6d4e05522535da98b44e6bcadfe1
Geoff <gabishop35@gmail.com>
2020-02-03 17:36:20 -0500

    step 2 complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let inputFields = document.querySelectorAll("input") 
+let inputFields = document.querySelectorAll("input")
 
 // let submitButton = document.querySelector("#submit-button")
 
@@ -8,70 +8,45 @@ let input = document.querySelectorAll(".input-field")
 
 
 
-parkingForm.addEventListener("submit", function(e) {
+parkingForm.addEventListener("submit", function (e) {
     e.preventDefault();
-    for(let field of inputFields) {
+    for (let field of inputFields) {
         if (field.value.length > 0) {
-            field.parentElement.classList.contains("input-valid")
-            
-        //  if  (field.parentElement.classList.add("input-valid"))
-        //         {}
-            }
+            field.parentElement.classList.add("input-valid")
+            let lastChild = field.parentElement.lastElementChild
+                if (lastChild.classList.contains("error")) {
+                    // console.log("error")
+                    lastChild.remove() 
+                }
+        }
         else {
+            field.parentElement.classList.remove("input-valid")
             field.parentElement.classList.add("input-invalid")
-
-            
+            let lastChild = field.parentElement.lastElementChild
+            if (lastChild.classList.contains("error")) {}
+            else {
             let newEl = document.createElement("div")
+            newEl.classList.add("error")
             let text = document.createTextNode("Required field")
-            let parent = field.parentNode 
+            let parent = field.parentNode
             newEl.appendChild(text)
             parent.appendChild(newEl)
-               
-                
-                }
+        }
+    }
+    }
 
-            // else {
-            // newEl.remove()
-}})
+})
 
 
-// parkingForm.addEventListener("submit", function(e) {
-//     e.preventDefault();
-//     for(let field of inputFields) {
-//         if (field.value.length === 0) {
-//         name.setAttribute("required", "")
-        // inputFields.innerText = "required"
-//         }
-//         // }
-//         //     field.parentElement.classList.add("input-invalid")  
-//         //     name.innerText = "required"
-//     }
-            
-// })
+
+// ***** parseInt *******
 
 // name.getAttribute()
 
 // name.attribute.required = true
 
-// inputFields.addEventListener("submit", function(){
-//     alert("is required")
-// })
-
 // let box = document.querySelector("#name-field")
-
-// box.classList.add("input-invalid")
-// // turn box red
-
 
 // let days = document.querySelector("#days-field")
 
-
-// // turn box green
-
-
-
-
-
-
 // check each box to see if there is any input, if there is then it is able to submit
-
